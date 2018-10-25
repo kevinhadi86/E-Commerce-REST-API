d0e3b3bbe33b6f45fb69064c40a548aafb116447
kevinhadi86 <kevinhadinata86@gmail.com>
2018-10-26 01:18:14 +0700

    seed product & review using faker

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -4,6 +4,10 @@ use Faker\Generator as Faker;
 
 $factory->define(App\Product::class, function (Faker $faker) {
     return [
-        //
+        'name'=>$faker->word,
+        'detail'=>$faker->sentence,
+        'price'=>$faker->numberBetween(100,1000),
+    	'stock'=> $faker->randomDigit,
+    	'discount'=>$faker->numberBetween(2,30)
     ];
 });
